ee6600d98b155d1799bd9197e1b2163a1245dc72
SeregaNag <71101232+SeregaNag@users.noreply.github.com>
2023-06-14 17:55:17 +0300

    File system is ready, env is ready

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { writeFile, access } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const create = async () => {
-    // Write your code here 
+  const filesDir = join(__dirname, "files", "fresh.txt");
+
+  try {
+    await access(filesDir);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      try {
+        await writeFile(filesDir, "I am fresh and young", "utf8");
+      } catch (error) {
+        console.error(error);
+      }
+    } else throw error;
+  }
 };
 
 await create();
