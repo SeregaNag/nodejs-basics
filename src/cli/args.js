8d5095e876ef1c87df365e995dd2ae4280164778
SeregaNag <71101232+SeregaNag@users.noreply.github.com>
2023-06-17 14:00:33 +0300

    args is ready

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const formattedArgs = args.map((arg, index) => {
+    if (index % 2 === 0) {
+      const prop = arg.slice(2);
+      const value = args[index + 1];
+      return `${prop} is ${value}`;
+    }
+  });
+
+  console.log(formattedArgs.join(", "));
 };
 
 parseArgs();
